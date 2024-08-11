757280e2351f9285b1c8982c5de391a5bb06fcd7
Janik <77326678+Janikfrb@users.noreply.github.com>
2024-08-11 11:50:24 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ cd /home/container
 
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-
+echo "I'm a Cat :3 Thank you for using Hosting mit Herz"
 # Safely replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
